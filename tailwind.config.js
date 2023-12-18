23e527272b8c1734a22f18c7dd86431c3d5ffdbe
tswmy <thipparko@gmail.com>
2023-12-18 17:41:12 +0530

    adding media project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        shimmer: {
+          "100%": { tranform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        shimmer: "shimmer 1.5s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
